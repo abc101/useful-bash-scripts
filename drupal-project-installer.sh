2f2d9b75bc8eedf7f995ed69979d1d1cb4ac74ee
Songmin Kim <abc101@gmail.com>
2017-02-19 11:51:56 -1000

    Added sudo option.

--- drupal-project-installer.sh
+++ drupal-project-installer.sh
@@ -7,23 +7,30 @@ This script is used to install drupal project by drush.
 you need to provide the following arguments:
 
   1) Project name.
-  2) Database name.
-  3) Database password.
-  4) Project account name
-  5) Project account password
+  2) database username.
+  3) Database name.
+  4) Database password.
+  5) Site name.
+  6) Project account name
+  7) Project account password
 
-Usage: bash ${0##*/} --project=PATH --db-user=DBUSER --db-pass=DBPASS --db-name=DBNAME --account-name=ACCOUNT --account-pass=PASSWORD
-Example: bash ${0##*/} --project=myproject --db-user=user --db-pass=mydbpas --db-name=mydb --account-name=myaccount -0account-pass=mypassword
+Usage: bash ${0##*/} --project=PATH --db-user=DBUSER --db-pass=DBPASS --db-name=DBNAME --site-name=SITE_NAME --account-name=ACCOUNT --account-pass=PASSWORD
+Example: bash ${0##*/} --project=myproject --db-user=user --db-pass=mydbpas --db-name=mydb --site-name=sitename --account-name=myaccount -account-pass=mypassword
 HELP
 exit 0
 }
+
+if [ $(id -u) != 0 ]; then
+  sudo="sudo"
+fi
 
 project=${1}
 dbuser=${2}
 dbpass=${3}
 dbname=${4}
-accname=${5}
-accpass=${6}
+site=${5}
+accname=${6}
+accpass=${7}
 
 # Parse Command Line Arguments
 while [ "$#" -gt 0 ]; do
@@ -39,6 +46,9 @@ while [ "$#" -gt 0 ]; do
         ;;
     --db-name=*)
         dbname="${1#*=}"
+        ;;
+    --site-name=*)
+        site="${1#*=}"
         ;;
     --account-name=*)
         accname="${1#*=}"
@@ -62,7 +72,7 @@ if [ -d "${project}" ]; then
   if [ "${a}" == "n" ]; then
     exit 1
   fi
-  sudo rm -r ${project}
+  ${sudo} rm -r ${project}
 fi
 
 printf "Downloading drupal core... \n\n"
@@ -70,13 +80,13 @@ drush dl drupal --drupal-project-rename=${project}
 printf "done.\n"
 printf "Change the drectory ownership... "
 chmod -R g+w ${project}
-sudo chown -R www-data:www-data ${project}
+${sudo} chown -R www-data:www-data ${project}
 printf "done.\n"
 printf "Installing $project... \n\n"
 cd ${project}
 mysql="mysql://${dbuser}:${dbpass}@localhost/${dbname}"
-drush site-install standard --db-url=${mysql} --account-name=${accname} --account-pass=${accpass}
+drush site-install standard --db-url=${mysql} --site-name=${site} --account-name=${accname} --account-pass=${accpass}
 printf "done.\n"
 printf "Change the default sites setting ownner ship... "
-sudo chown -R www-data:www-data sites/default
+${sudo} chown -R www-data:www-data sites/default
 printf "done.\n"
